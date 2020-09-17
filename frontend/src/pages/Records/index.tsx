763505f0e0b88e5fa88baee7050def1585f700ba
Gagego Maciel <gagego.maciel@gmail.com>
2020-09-16 21:12:32 -0300

    Adding the graphics page

--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+
 
 import { RecordsResponse } from './types';
 import { formatDate } from './helpers';
 import Pagination from './Pagination';
+import Filters from '../../components/Filters';
 
 import './styles.css';
 
@@ -15,7 +16,7 @@ const Records = () => {
   const [activePage, setActivePage] = useState(0);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/records?linesPerPage=15&page=${activePage}`)
+    axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
       .then(response => setRecordsResponse(response.data));
   }, [activePage]);
 
@@ -25,13 +26,10 @@ const Records = () => {
 
   return (
     <div className="page-container">
-      <div className="filters-container records-actions">
-        <Link to="/charts">
-          <button className="action-filters"> 
-            VER GRÁFICOS
-          </button>
-        </Link>
-      </div>
+      <Filters 
+        link="/charts"
+        linkText="VER GRÁFICOS"
+      />
       <table className="records-table" cellPadding="0" cellSpacing="0">
         <thead>
           <tr>
